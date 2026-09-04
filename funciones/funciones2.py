fdd9e0003f4b5b420ea36f969ee0f9f61a5704cd
emanueljmendoza-a11y <emanueljmendoza@uamv.edu.ni>
2026-09-04 08:14:38 -0600

    Update funciones2

--- funciones/funciones2.py
+++ funciones/funciones2.py
@@ -2,5 +2,15 @@
 def sumar(numero1, numero2):
     return numero1 + numero2
 
+def restar(numero1=0, numero2=2):
+    return numero1 - numero2
+
 suma= sumar(15, 17)
-print(suma)
+resta= restar(4, 2)
+
+print(resta)
+resta = restar (numero2=6,numero1=2)
+print(resta)
+
+resta = restar (8)
+print(resta)
