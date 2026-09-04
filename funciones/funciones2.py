b48b232e15c733da83e77af8e13bf4a642d8adb2
emanueljmendoza-a11y <emanueljmendoza@uamv.edu.ni>
2026-09-04 08:40:12 -0600

    Update funciones2

--- funciones/funciones2.py
+++ funciones/funciones2.py
@@ -1,16 +1,29 @@
-#sumar dos numeros
+#Sumar 2 numeros
 def sumar(numero1, numero2):
     return numero1 + numero2
 
-def restar(numero1=0, numero2=2):
+suma = sumar(15, 17)
+print(suma)
+
+def restar(numero1 = 0, numero2 = 2):
     return numero1 - numero2
 
-suma= sumar(15, 17)
-resta= restar(4, 2)
+resta = restar(4, 2)
+print(resta)
+resta = restar(numero2= 6, numero1 = 2)
+print(resta)
+resta = restar(8)
+print(resta)
 
-print(resta)
-resta = restar (numero2=6,numero1=2)
-print(resta)
 
-resta = restar (8)
-print(resta)
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
+
+def dividir(numero1, numero2):
+    try:
+        return numero1 / numero2
+    except ZeroDivisionError:
+        return "El segundo valor debe ser mayor que 0"
+
+print(multiplicar(4, 2))
+print(dividir(20, 5))
